64e8673de31e0e2252c64a4f4c2773cebd8f28f5
Jhey Marc Abad <149645647+ShepherdBoyy@users.noreply.github.com>
2025-09-09 15:50:21 +0800

    Using preload scripts

--- preload.js
+++ preload.js
@@ -1,8 +1,8 @@
-const { contextBridge } = require('electron')
+const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('versions', {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron
-  // we can also expose variables, not just functions
+  electron: () => process.versions.electron,
+  ping: () => ipcRenderer.invoke('ping')
 })
